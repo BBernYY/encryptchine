272537d8fa6cefbb826b7ba6ce6106ac9f9dc0f8
BBernYY <66414852+BBernYY@users.noreply.github.com>
2021-09-26 13:45:38 +0200

    made it cooler with photos and secrets

--- binaryCode.py
+++ binaryCode.py
@@ -1,6 +1,9 @@
 import random
+import os
 def decode(key, filename):
     f = open(filename, "rb")
+    iter = f.read()
+    f.seek(iter.index(bytes.fromhex("FFD9")) + 2)
     byte = f.read(1)
     word = key
     word = [char for char in word]
@@ -28,7 +31,16 @@ def encode(text, key, use_file="none"):
         else:
             print("Unknown character.")
             break
-    return bytes(numbers)
+    name = "photos//"+ str(random.randint(0, 4)) + ".jpeg"
+    file2 = open(name, 'rb')
+    file = open("photo.jpeg", "wb")
+    file.write(file2.read())
+    file.close()
+    file2.close()
+    file = open("photo.jpeg", "ab")
+    file.write(bytes(numbers))
+    file.close()
+    return open("photo.jpeg", "r")
 def generateKey(log):
     needed = [char for char in "`1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:|ZXCVBNM<>? ⻲⻕"]
     key = "".join(random.sample(needed, len(needed)))
--- execution script.py
+++ execution script.py
@@ -3,10 +3,8 @@ import pyperclip
 import time
 a = input("encode, decode, or make a key[e/d/m]?\n")
 if a == "e":
-    f = open(input("filename?\n"), "wb")
-    f.write(bc.encode(input("what text?\n"), input("what key?\n")))
-    print("file made.")
-    f.close()
+    bc.encode(input("what text?\n"), input("what key?\n"))
+    print("file saved as photo.jpeg.\nRename for extra security.")
 elif a == "d":
     print(bc.decode(input("what key?\n"), input("filename?\n")))
     print("that's the code.")
